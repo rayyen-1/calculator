f8988c8ab60581154ff696dad34dc7bdb7a1a20b
rayyen-1 <raina.ray.me@gmail.com>
2026-04-07 15:51:54 +0700

    adding vars and methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int a = 10, b = 6;
+
+static int Penjumlahan(int a, int b)
+{
+    return a + b;
+}
+static int Pengurangan(int a, int b)
+{
+    return a - b;
+}
+static int Perkalian(int a, int b)
+{
+    return a * b;
+}
+static int Pembagian(int a, int b)
+{
+    return a / b;
+}
+
+Console.WriteLine(a+ "+" +b+ "=" +Penjumlahan(a,b));
+Console.WriteLine(a+ "-" +b+ "=" +Pengurangan(a,b));
+Console.WriteLine(a+ "*" +b+ "=" +Perkalian(a,b));
+Console.WriteLine(a+ "/" +b+ "=" +Pembagian(a,b));
